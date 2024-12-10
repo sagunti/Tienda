3a76e0f76e2ba5c5cdbb64a7d77c40981549adc6
sagunti <seralemon1@gmail.com>
2024-12-10 20:18:27 -0300

    productos con json

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+fetch("./posts.json")
+  .then((response) => {
+    // console.log(response);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+
+    return response.json();
+  })
+  .then((posts) => {
+    const section = document.querySelector("[data-item]");
+
+    section.innerHTML = "";
+
+    posts.forEach((post) => {
+      const html = `
+      
+              <div class="item">
+            
+              <img class="imagenItem"id="${post.title}" src="../assets/imagenes/reverso.png" alt="${post.title}">
+              <h2 class="TituloItem">${post.title}</h2>
+              <h3>${post.body}</h3>
+              <p> € ${post.precio}</p>
+              <button>Comprar</button>
+              
+          </div>
+           
+        `;
+
+      section.innerHTML += html;
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
